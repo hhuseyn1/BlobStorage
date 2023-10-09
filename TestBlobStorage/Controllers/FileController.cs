46d20420c2ffc495a904fe0610e3687658fe8d13
Huseyn <huseyn.hemidov.2004@gmail.com>
2023-10-09 19:13:05 +0400

    Add project files.

---
+++ TestBlobStorage/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TestBlobStorage.Services;
+
+namespace TestBlobStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IStorageManager _storageManager;
+
+        public FileController(IStorageManager storageManager)
+        {
+            _storageManager = storageManager;
+        }
+
+        [HttpGet("getUrl")]
+        public IActionResult GetUrl(string fileName)
+        {
+            try
+            {
+                var result = _storageManager.GetSignedUrl(fileName);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
